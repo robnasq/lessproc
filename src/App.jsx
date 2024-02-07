c04b69c6064386cf13527412212b8fb0e350365d
robnasq <roberto.nasc93@gmail.com>
2024-02-07 18:40:10 -0300

    Modificações no novo portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import logo from './assets/images/logo.svg';
 import { Home } from './pages/home/Home';
 
 function App() {
   return (
     <div className="app">
-    <Home />
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p className="header">Vite React Starter 💯</p>
-        <p>
-          Vite + React <br />
-          ESLint + Prettier + Stylelint
-          <br />
-          Sass + Emotion + Tailwind
-          <br />
-          Jest + Testing Library
-        </p>
-      </header>
+      <Home />
     </div>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export const Home = () => {
   return (
     <div>
-      <div>home</div>
+      <div className="flex h-[100vh] items-center justify-center bg-black">
+        <div className="leadim-[1em] pb-[120px] pt-[80px] text-[160px] text-white  sm:text-[72px]">
+          <h1>
+            Front-End <br /> Developer
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
