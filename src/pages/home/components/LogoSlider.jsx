add92500b4b83c2aed58b75dc9b25a0189a5cada
robnaq <roberto.nasc93@gmail.com>
2024-02-10 11:59:45 -0300

    Logos added

--- src/pages/home/components/LogoSlider.jsx
+++ src/pages/home/components/LogoSlider.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
+import HtmlLogo from '../assets/html-logo.jpeg';
+import JsLogo from '../assets/js-logo.jpeg';
+import ReactLogo from '../assets/react-logo.jpeg';
+import TawilwindLogo from '../assets/tailwind-logo.jpeg';
+import TsLogo from '../assets/ts-logo.jpeg';
+
 export const LogoSlider = () => {
   return <div >
-    <Marquee className='mt-[140px]'> 
-      <div className='w-[100px] h-12 bg-slate-300 '></div>
+    <Marquee className="mt-[140px]">
+      <div className="w-[100px] h-12 bg-slate-300 " />
+      <img src={ReactLogo} />
     </Marquee>
   </div>;
 };
